38d187d42c06cdfa1c89191a0dd1a8abb4f16a4f
Daniel Gutiérrez <alu0100695595@ull.edu.es>
2013-11-06 16:59:50 +0000

    Versión inicial con implementación provisional de la clase

---
+++ lib/matrix.rb
@@ -0,0 +1,62 @@
+class Matriz
+  include Comparable
+  attr_reader :matriz, :filas, :columnas
+   
+  def initialize(filas, columnas)
+    @filas = filas
+    @columnas = columnas
+    @matriz = Array.new(filas)
+  end
+
+  def filas
+    @filas
+  end
+
+  def columnas
+    @columnas
+  end
+
+  def +(other)
+
+    if((@rows == other.rows) && (@columns == other.columns))
+      result = Matriz.new(filas, columnas)
+
+      for i in (0...filas)
+	for j in (0...columnas)
+	  result[i][j] = self[i][j] + other[i][j]
+	end
+      end
+    end
+  end
+
+  def -(other)
+
+    if((@rows == other.rows) && (@columns == other.columns))
+      result = Matriz.new(filas, columnas)
+
+      for i in (0...filas)
+	for j in (0...columnas)
+	  result[i][j] = self[i][j] + other[i][j]
+	end
+      end
+
+    end
+  end
+
+  def *(other)
+
+
+  end
+
+  def /(other)
+
+
+  end
+
+  def to_s
+
+  end
+
+  
+
+end
