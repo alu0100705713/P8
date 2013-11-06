a8cc48f249e286233d45a073f7bd260e0a3b9393
Ramon Mesa <alu4429@cc412.(none)>
2013-11-06 18:54:29 +0000

    Versión con suma, resta y test correspondientes

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -1,62 +1,68 @@
+# encoding: UTF-8
   class Matriz
+
     include Comparable
+    
     attr_reader :matriz, :filas, :columnas
     
     def initialize(filas, columnas)
       @filas = filas
       @columnas = columnas
-      @matriz = Array.new(filas)
+      @matriz = Array.new(filas){ Array.new(columnas, rand(10)) }      
     end
-  
+
+    def to_s
+      
+      for i in 0...@filas
+	puts "#{@matriz[i]}"
+      end
+      
+    end
+    
     def filas
-      @filas
+      return @filas
     end
   
     def columnas
-      @columnas
+      return @columnas
     end
   
+      def [](filas, columnas = nil)
+		  if(columnas.nil?)
+                        @matriz[filas]
+		  else
+                        @matriz[filas][columnas]
+		  end
+	  end      
+    
     def +(other)
   
-      if((@rows == other.rows) && (@columns == other.columns))
-	result = Matriz.new(filas, columnas)
-  
-	for i in (0...filas)
-	  for j in (0...columnas)
-	    result[i][j] = self[i][j] + other[i][j]
+      if((@filas == other.filas) && (@columnas == other.columnas))
+	resultado = Matriz.new(@filas, @columnas)
+	  for i in 0...@filas
+	    for j in 0...@columnas
+	      resultado[i][j] = self[i][j] + other[i][j]
+	    end
 	  end
-	end
+      else
+	puts "Las matrices no tienen el mismo número de filas y columnas, no se puede sumar"
       end
+      return resultado
     end
-  
+
     def -(other)
   
-      if((@rows == other.rows) && (@columns == other.columns))
-	result = Matriz.new(filas, columnas)
-  
-	for i in (0...filas)
-	  for j in (0...columnas)
-	    result[i][j] = self[i][j] + other[i][j]
+      if((@filas == other.filas) && (@columnas == other.columnas))
+	resultado = Matriz.new(@filas, @columnas)
+	  for i in 0...@filas
+	    for j in 0...@columnas
+	      resultado[i][j] = self[i][j] - other[i][j]
+	    end
 	  end
-	end
-  
+      else
+	puts "Las matrices no tienen el mismo número de filas y columnas, no se puede restar"
       end
+      return resultado
     end
-  
-    def *(other)
-  
-  
-    end
-  
-    def /(other)
-  
-  
-    end
-  
-    def to_s
-  
-    end
-  
-    
-  
+
   end
---
+++ spec/matriz_spec.rb
@@ -0,0 +1,61 @@
+require 'matrix.rb'
+
+  describe Matriz do
+	before :each do
+	    @p1 = Matriz.new(3,3)  
+	    @p2 = Matriz.new(3,3)
+	    @p3 = @p1 + @p2
+	    @p4 = @p1 - @p2
+	end
+
+      describe "Variables" do
+	
+	  it "Comprobación de creación de matrices" do
+	    @p1.is_a? Array
+	    @p2.is_a? Array
+	    @p3.is_a? Array
+	    @p4.is_a? Array
+	  end
+	  
+	  it "Comprobación existencia filas y columnas" do
+	    @p1.filas.should_not be_nil 
+	    @p1.columnas.should_not be_nil 
+	    @p2.filas.should_not be_nil 
+	    @p2.columnas.should_not be_nil 
+	  end
+	  
+	  it "Comprobación de creación y acceso a las variables" do 
+	    @p1[0][0].should_not be_nil
+	    @p1[0][1].should_not be_nil
+	    @p1[0][2].should_not be_nil
+	    @p1[1][0].is_a? Integer
+	    @p1[1][1].is_a? Integer
+	    @p1[1][2].is_a? Integer
+	    @p1[2][0].is_a? Integer
+	    @p1[2][1].is_a? Integer
+	    @p1[2][2].is_a? Integer
+	  end
+	  
+      end
+
+      describe "Operaciones" do	
+
+	 it "La suma da enteros" do
+	  for i in 0...3
+	    for j in 0...3
+	      @p3[i][j].is_a? Integer
+	    end
+	  end	  
+	 end
+	
+	 it "La resta da enteros" do
+	  for i in 0...3
+	    for j in 0...3
+	      @p4[i][j].is_a? Integer
+	    end
+	  end	  
+	end
+      end
+
+	  
+      end
